ab7b000d36ec606b93722b6a67bfcb9e543be2ff
millet.ua.su@gmail.com <millet.ua.su@gmail.com>
2019-04-26 21:44:37 +0300

    install angular app

--- MyRoomFront/my-room/README.md
+++ MyRoomFront/my-room/README.md
@@ -1,4 +1,4 @@
-# MyRoomFront
+# MyRoom
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.8.
 
--- MyRoomFront/my-room/src/app/app.component.spec.ts
+++ MyRoomFront/my-room/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-room-front'`, () => {
+  it(`should have as title 'my-room'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-room-front');
+    expect(app.title).toEqual('my-room');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-room-front!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-room!');
   });
 });
